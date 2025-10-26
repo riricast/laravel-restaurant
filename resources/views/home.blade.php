a41f9eeb18d7377d7c5da95ec838b2a82bbb42fc
riricast <rihannahkyliecastillo@gmail.com>
2025-10-26 23:04:07 +0800

    Addition of Home Page and route

---
+++ resources/views/home.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+
+    @vite(['resources/css/app.css', 'resorces/js/app.css'])
+</head>
+<body>
+    <h1>hello world</h1>
+    <p>this is a test paragraph to test if everything is working</p>
+</body>
+</html>
